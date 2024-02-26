0330a42aff904194de2a5153e0b7c7228318255c
HuslT1me <152012295+HuslT1me@users.noreply.github.com>
2024-02-26 14:31:50 +0200

    first relise

--- js/dropdown-location.js
+++ js/dropdown-location.js
@@ -1,49 +1,53 @@
+const choicesElems = Array.from(document.querySelectorAll('.choices'));
 
-const choicesElement = document.querySelector('.choices');
-const choicesInner = document.querySelector('.choices__inner');
-const choicesList = document.querySelector('.choices__list--dropdown');
-const choicesSingleItem = document.querySelector('.choices__list--single .choices__item');
-const choicesDropdownList = Array.from(document.querySelectorAll('.choices__list--dropdown .choices__item'));
+choicesElems.forEach(item => {
+  item.addEventListener('click', () => {
+    if(!item.className.includes('is-open')){
+      openSelect(item); 
+      const choicesSingleItem = item.querySelector('.choices__list--single .choices__item');
+      const dropList = Array.from(item.querySelectorAll('.choices__list--dropdown .choices__item'));
+      dropList.forEach(el => {
+        el.addEventListener('click', (evt) => {
+          selectItem(evt.target, item, choicesSingleItem, dropList);
+        })
+      })
+    } else {
+      closeSelect(item);
+    }
+  })
+});
 
-choicesElement.addEventListener('click', openLocationList);
+function openSelect(item){
+  item.classList.add('is-open');
+  let n = "true" === item.getAttribute("aria-expanded");
+  item.setAttribute("aria-expanded", !n);
+  item.querySelector('.choices__list--dropdown').classList.add('is-active');
+};
 
-// Открываем список локаций
+function closeSelect(item){
+  item.classList.remove('is-open');
+  let n = "true" === item.getAttribute("aria-expanded");
+  item.setAttribute("aria-expanded", !n);
+  item.querySelector('.choices__list--dropdown').classList.remove('is-active');
+};
 
-function openLocationList() {
-  choicesElement.classList.toggle('is-open');
-  let n = "true" === choicesElement.getAttribute("aria-expanded");
-  choicesElement.setAttribute("aria-expanded", !n);
-  choicesList.classList.toggle('is-active');
-}
-
-choicesDropdownList.forEach(item => {
-  item.addEventListener('click', selecteDropdownItem );
-})
-
-// Выбираем элемент из выпадающего списка 
-
-function selecteDropdownItem(evt) {
-  if(evt.target.dataset.value !== choicesSingleItem.dataset.value) {
-    choicesDropdownList.forEach(item => {
-      item.classList.remove('is-selected');
-      item.classList.add('is-highlighted');
+function selectItem(target, item, single, list){
+  if(target.dataset.value !== single.dataset.value) {
+    list.forEach(elem => {
+      elem.classList.remove('is-selected');
+      elem.classList.add('is-highlighted');
     })
-    evt.target.classList.add('is-selected');
-    evt.target.classList.remove('is-highlighted');
-    choicesSingleItem.textContent = evt.target.textContent;
-    choicesSingleItem.dataset.value = evt.target.dataset.value;
+    target.classList.add('is-selected');
+    target.classList.remove('is-highlighted');
+    single.textContent = target.textContent;
+    single.dataset.value = target.dataset.value;
   }
 }
-
-// Отслеживаем клик вне выпадающего списка
 
 window.addEventListener('click', (evt) => {
-  console.log();
-  if(!evt.target.closest('.choices') && choicesElement.className.includes('is-open')) {
-    console.log('hi');
-    choicesElement.classList.remove('is-open');
-    let n = "true" === choicesElement.getAttribute("aria-expanded");
-    choicesElement.setAttribute("aria-expanded", !n);
-    choicesList.classList.remove('is-active');
-  }
+    if(!evt.target.closest('.choices')) {
+      choicesElems.forEach(item => {
+        closeSelect(item);
+      })
+    }
 })
--- js/menu-toggle-misc.js
+++ js/menu-toggle-misc.js
@@ -1,8 +1,11 @@
 function menuToggle () {
-	$('.js-menu').toggleClass('is-active')
+	$('.js-menu').toggleClass('is-active');
+	$('.bg-menu').toggleClass('active');
 	$('.mobile-menu').toggleClass('opened');
 }
 
-$( document ).ready(function() {
-	$('.js-menu').click(menuToggle);
-});
+const btns = document.querySelectorAll('.js-menu');
+
+btns.forEach(item => {
+	item.addEventListener('click', menuToggle)
+})
