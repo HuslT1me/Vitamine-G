75fe58d11867a752245ba0e13e44a545e9f11e49
HuslT1me <152012295+HuslT1me@users.noreply.github.com>
2024-03-19 13:37:15 +0200

    test tabs

--- js/dropdown-misk.js
+++ js/dropdown-misk.js
@@ -2,6 +2,30 @@
 
 const miskParagrafList = Array.from(document.querySelectorAll('.misc__aside-list p'));
 const miskLinks = Array.from(document.querySelectorAll('.misc__aside-list a'));
+const miscTextEl = document.querySelector('.misc__text');
+console.log(miscTextEl);
+
+$('.nav__link').on('click', function(e){
+    e.preventDefault();
+
+	const listActiveElems = Array.from(document.querySelectorAll('.misc__aside-list.list-active'));
+	listActiveElems.forEach(item => {
+		item.classList.remove('list-active');
+	});
+
+    $('.nav__link').removeClass('tab--active');
+    $('.misc__aside').removeClass('misc__aside--active');
+
+	$('.misc__aside-list:first-child').addClass('list-active');
+
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('misc__aside--active')
+	
+	const miscAsideActiveEl = document.querySelector('.misc__aside--active');
+	const linkActiveElems = miscAsideActiveEl.querySelector('.misc__aside-list.list-active a');
+	linkActiveElems.classList.add('link-active');
+})
+
 
 miskParagrafList.forEach(list => {
 	list.addEventListener('click', openList)
@@ -17,7 +41,7 @@ function openList(evt) {
 }
 
 function isActiveLink(evt){
-	evt.preventDefault();
+	// evt.preventDefault();
 	miskLinks.forEach(link => {
 		link.classList.remove('link-active');
 	})
--- js/menu-toggle-misc.js
+++ js/menu-toggle-misc.js
@@ -9,3 +9,12 @@ const btns = document.querySelectorAll('.js-menu');
 btns.forEach(item => {
 	item.addEventListener('click', menuToggle)
 })
+
+const menuBottomElem = document.querySelector('.mobile-menu__bottom-row');
+const menuItemElems = Array.from(menuBottomElem.querySelectorAll('.nav__link'));
+
+menuItemElems.forEach(item => {
+	item.addEventListener('click', () => {
+		$('.mobile-menu').removeClass('opened');
+	})
+})
